4eaaf2449b67ffc0d4056448de53aab54c401cfe
cliche <745356675@qq.com>
2021-11-30 20:30:33 +0800

    -kmeans done

--- lab_6/k_means.py
+++ lab_6/k_means.py
@@ -14,8 +14,6 @@ class Kmeans(object):
     def train(self):
         x = self.training_x
         epsilon = 1e-5
-        pre_mu = np.zeros(x.shape[0])
-
         loop_count = 0
         # 最多loop 100次
         for loop in trange(100):
@@ -37,16 +35,21 @@ class Kmeans(object):
         return loop_count
 
     def reassign(self, img):
-        img.astype(np.float64)
+        new_img = img.copy().astype(np.float64)
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
-                k = np.argmin(linalg.norm(img[i][j] - self.mu, axis=1, keepdims=True) ** 2)
-                img[i][j] = self.mu[k]
+                k = np.argmin(linalg.norm(new_img[i][j] - self.mu, axis=1, keepdims=True) ** 2)
+                new_img[i][j] = self.mu[k]
+
+        return new_img
 
 if __name__ == "__main__":
     large_img = cv2.imread('data6/bird_large.tiff')
     small_img = cv2.imread('data6/bird_small.tiff')
-    kmeans = Kmeans(small_img)
-    kmeans.train()
-    kmeans.reassign(large_img)
-    cv2.imwrite('data6/bird_large_after_all.tiff', np.uint8(np.round(large_img)))
+    for k in [16, 32, 64, 128]:
+        print(f'{k} bit colors')
+        for i in range(10):
+            kmeans = Kmeans(small_img, k=k)
+            kmeans.train()
+            new_img = kmeans.reassign(large_img)
+            cv2.imwrite('data6/bird_large_after_{}_with_k_{}.tiff'.format(i, k), np.uint8(np.round(new_img)))
